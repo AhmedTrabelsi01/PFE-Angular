16d6566a5f1f3ffcfae6852d4c170b5d20f9e30e
AhmedTrabelsi01 <73963512+AhmedTrabelsi01@users.noreply.github.com>
2023-04-04 10:30:03 +0100

    drag and drop board

--- src/app/pages/djagora-academy/scrum/boards/boards.component.ts
+++ src/app/pages/djagora-academy/scrum/boards/boards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+declare function popup():any ;
 
 @Component({
   selector: 'app-boards',
@@ -10,6 +12,48 @@ export class BoardsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  popup()
+  }
+ 
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  review = [
+    'Take bath',
+    'Wash car',
+  ];
+  list1:any=['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum',' a dignissim dolor malesuada.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada.'];
+  list2:any=[ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum',' a dignissim dolor malesuada.','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada.'];
+  list3=[ "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada."];
+  list4=["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum velit velerat fermentum, a dignissim dolor malesuada."];
+
+  drop(event: CdkDragDrop<string[]>) {
+  
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+        transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+    
+   
+
   }
 
 }
